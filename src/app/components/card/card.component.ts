0fcaea87ccf6ba7f4715e09d891c27fa400a1d83
Tiago <tiago@gobacklog.com>
2021-07-02 09:39:57 -0300

    Wishlist

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
+import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -8,10 +9,23 @@ import { Product } from 'src/app/models/product.model';
 export class CardComponent implements OnInit {
 
   @Input() product: Product;
+  public productsSaved: string[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    ProductService.emitProductSave.subscribe(
+      res => {
+        this.productsSaved = res;
+        console.log('products saved', this.productsSaved);
+      }
+    );
+
+  }
+
+  saveProduct(id: string) {
+    this.productsSaved.push(id);
+    ProductService.emitProductSave.emit(this.productsSaved);
   }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
@@ -8,10 +8,12 @@ import { Search } from '../models/search.model';
 })
 export class ProductService {
 
+  static emitProductSave = new EventEmitter<string[]>();
+
   constructor(private http: HttpClient) { }
 
   getByStoreId(storeId: string): Observable<any> {
-    return this.http.get(`${environment.baseApi}/stores/${storeId}/products`);
+    return this.http.get(`${environment.baseApi}/stores/${storeId}/products?_sort=id`);
   }
 
   get(id: string): Observable<any> {
