92cf24ccafbe513a8985b02b2a4e5e893cb16f92
Tiago Facco <thifacco@gmail.com>
2023-03-14 21:53:52 +0000

    chore: refactor header

--- src/app/pages/shared/shared.module.ts
+++ src/app/pages/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 
 @NgModule({
   declarations: [CardComponent, HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports: [CardComponent, HeaderComponent]
 })
