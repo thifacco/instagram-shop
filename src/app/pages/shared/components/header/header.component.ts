db9bb811bb10bd1aef0b12a28c4bec8f3450422b
Tiago <tiago@gobacklog.com>
2023-03-30 22:21:30 -0300

    chore: new top menu header

--- src/app/pages/shared/components/header/header.component.ts
+++ src/app/pages/shared/components/header/header.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { iHeader } from 'src/app/interfaces/header.interface';
+import { environment } from 'src/environments/environment';
+
+interface navMenu {
+  name: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-header',
@@ -9,6 +15,18 @@ import { iHeader } from 'src/app/interfaces/header.interface';
 export class HeaderComponent implements OnInit {
 
   @Input() headerData: iHeader;
+
+  absLinkHome: navMenu = {
+    name: 'Home',
+    url: `${environment.hostname}/home`
+  };
+
+  absLinkWishlist: navMenu = {
+    name: 'Wishlist',
+    url: `${environment.hostname}/wishlist`
+  };
+
+  topNavMenuItems = [{...this.absLinkHome}, {...this.absLinkWishlist}];
 
   constructor() { }
 
