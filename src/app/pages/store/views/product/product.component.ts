2f7aff7a0c33763cfda4492ae348f460f80dd7ca
Tiago <tiago@gobacklog.com>
2023-03-31 18:38:52 -0300

    feat(store): get product data

--- src/app/pages/store/views/product/product.component.ts
+++ src/app/pages/store/views/product/product.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product.model';
+import { Store } from 'src/app/models/store.model';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  store: Store;
+  product: Product;
+
+  constructor(
+    public route: ActivatedRoute,
+    public storeService: StoreService
+  ) { }
 
   ngOnInit(): void {
+    const getParamStoreId = this.route.snapshot.params['storeId'];
+    const getParamProducId = this.route.snapshot.params['productId'];
+
+    if (getParamStoreId !== undefined && getParamProducId !== undefined) {
+      this.storeService.getById(getParamStoreId).subscribe((data) => {
+        if (data[0]) {
+          this.store = data[0];
+          this.product = this.store.products.filter((product) => product.id === getParamProducId)[0];
+        }
+      })
+    }
   }
 
 }
