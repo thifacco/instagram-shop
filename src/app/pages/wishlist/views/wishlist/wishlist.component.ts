1a30a0e4967e71a10dabe6943a52a57514889cea
Tiago <tiago@gobacklog.com>
2023-06-03 23:26:54 -0300

    feat(wishlist): view

--- src/app/pages/wishlist/views/wishlist/wishlist.component.ts
+++ src/app/pages/wishlist/views/wishlist/wishlist.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { IHeader } from 'src/app/interfaces/header.interface';
+import { LocalstorageService } from 'src/app/services/localstorage.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-wishlist',
@@ -13,8 +16,28 @@ export class WishlistComponent implements OnInit {
     linkBack: '/'
   }
 
-  constructor() { }
+  public productsWishlist: object[] = [];
+  private subscription: Subscription;
 
-  ngOnInit(): void { }
+  constructor(
+    private localstorageService: LocalstorageService,
+    private productService: ProductService
+  ) { }
+
+  ngOnInit(): void {
+    this.getProductsWishlist();
+  }
+
+  async getProductsWishlist() {
+    const items: string[] = this.localstorageService.get('wishlist');
+    await items.map(productId => {
+      this.subscription =
+        this.productService.get(productId).subscribe(res => this.productsWishlist.push(res))
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
 }
